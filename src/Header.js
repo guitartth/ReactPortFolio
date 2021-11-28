4c72e5d11cb6eb5af6bc4452bb1ce8f33ab883b8
Craig Freeburg <craigfreeburg@gmail.com>
2021-11-28 16:20:04 -0600

    updating small changes in css and header

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+//import Button from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            <img src="/assets/imgs/CraigHeaderSW2.png" height="150px" className="craig_header_img" alt="Craig Cartoon Face"></img>
+            <img src="/assets/imgs/CraigHeader.png" height="150px" className="craig_header_img" alt="Craig Cartoon Face"></img>
             <nav id="nav">
-                <a href="google.com"><button className="nav_btn" id="me_nav_btn" >Me</button></a>
-                <a href="google.com"><button className="nav_btn" id="projects_nav_btn" >Projects</button></a>
-                <a href="google.com"><button className="nav_btn" id="find_me_nav_btn" >Find Me</button></a>
-                <a href="google.com"><button className="nav_btn" id="contact_nav_btn" >Contact</button></a>
+                <button className="nav_btn" id="me_nav_btn" disabled="disabled">m<br/>e</button>
+                <button className="nav_btn" id="projects_nav_btn" disabled="disabled">p<br/>r<br/>o<br/>j<br/>e<br/>c<br/>t<br/>s</button>
+                <button className="nav_btn" id="find_me_nav_btn" disabled="disabled">f<br/>i<br/>n<br/>d<br/> <br/>m<br/>e</button>
+                <button className="nav_btn" id="contact_nav_btn" disabled="disabled">c<br/>o<br/>n<br/>t<br/>a<br/>c<br/>t</button>
             </nav>
         </header>
     )
